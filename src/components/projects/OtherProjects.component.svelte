15a24212bef8f2299f7fbd4d8fa3ff54db5547db
i.kłusek <J6ZkqwVUn>
2024-11-03 17:56:31 +0100

    PA-46-Added data about projects, added component for rest of the projects, create separate component for links

---
+++ src/components/projects/OtherProjects.component.svelte
@@ -0,0 +1,197 @@
+<script lang="ts">
+  import Link from './link.component.svelte';
+
+  export let nd2: boolean = false; // index n2 will have other style properties
+
+  let title = 'Pozostałe projekty';
+  let description =
+    'Zapraszam do odkrycia moich pozostałych projektów! Odwiedź mój profil na GitHubie, aby zobaczyć pełen zakres moich umiejętności oraz zrealizowanych przedsięwzięć. Przekonaj się sam, jak różnorodne są moje projekty i jakie technologie wykorzystałem w ich realizacji!';
+</script>
+
+<div class="wrapper" class:wrapper-nd2={nd2}>
+  <div class="content-wrapper">
+    <h1>{title}</h1>
+    <p>{description}</p>
+    <ul>
+      <li>
+        <p><span>LinkedinApp</span> (Angular, Typescript, NestJs, Socket.io)</p>
+        <Link
+          link="https://github.com/IamIGI/LinkedinApp"
+          name="Github"
+          {nd2}
+        />
+      </li>
+      <li>
+        <p><span>Interactive Comments Section</span> (React, Redux)</p>
+        <Link
+          link="https://github.com/IamIGI/Interactive-comments-section-FRONTEND"
+          name="Github"
+          {nd2}
+        />
+      </li>
+      <li>
+        <p><span> Multi Step Form</span> (Svelte, SCSS, RWD)</p>
+        <Link
+          link="https://github.com/IamIGI/Multi-step-form"
+          name="Github"
+          {nd2}
+        />
+      </li>
+      <li>
+        <p><span>IP Address Tracker</span> (Svelte, Leaflet-map, Sass)</p>
+        <Link
+          link="https://github.com/IamIGI/ip-address-tracker"
+          name="Github"
+          {nd2}
+        />
+      </li>
+    </ul>
+
+    <div class="image-wrapper">
+      <div class="image-content" class:image-content-2nd={nd2}>
+        <img src="/images/projects/other_projects.JPG" alt="project" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  $first-tile-background-color: #232121;
+  $first-font-color: #706e6c;
+  $first-font-title-color: #c3c1c0;
+
+  $second-tile-background-color: #edebe8;
+  $second-font-color: #959392;
+  $second-font-title-color: #4f4d4c;
+
+  $border-radius: 20px;
+
+  .wrapper {
+    width: 100%;
+    max-width: 1300px;
+    padding: 40px 6%;
+    border-radius: $border-radius;
+    background-color: $first-tile-background-color;
+    gap: 1rem;
+
+    h1 {
+      color: $first-font-title-color;
+      margin-bottom: 10px;
+    }
+
+    p {
+      color: $first-font-color;
+      /* margin-bottom: 10px; */
+    }
+
+    ul {
+      list-style-type: circle; /* Add circle style */
+
+      li {
+        /* margin-bottom: 10px; */
+        /* outline: 1px solid red; */
+        /* height: 60px; */
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 1rem;
+        p {
+          margin-top: 0px;
+          /* outline: 1px solid blue; */
+          span {
+            color: $first-font-title-color;
+          }
+        }
+      }
+    }
+  }
+
+  .wrapper-nd2 {
+    background-color: $second-tile-background-color;
+
+    h1 {
+      color: $second-font-title-color;
+    }
+
+    p {
+      color: $second-font-color;
+    }
+  }
+
+  .content-wrapper {
+    flex: 1;
+    padding-bottom: 25px;
+  }
+
+  .image-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    margin-top: 1rem;
+
+    .image-content {
+      width: 100%;
+      border-radius: $border-radius;
+
+      img {
+        height: 100%;
+        width: 100%;
+        border-radius: $border-radius;
+      }
+    }
+
+    .image-content-2nd {
+      box-shadow: 11px -7px 0px rgba(214, 212, 210, 1);
+    }
+  }
+
+  @media (max-width: 1210px) {
+    .wrapper {
+      padding: 3% 4%;
+      h1 {
+        font-size: 41px;
+      }
+      p {
+        font-size: 15px;
+      }
+    }
+  }
+
+  @media (max-width: 850px) {
+    .wrapper {
+      max-width: 620px;
+      flex-direction: column-reverse;
+      padding: 6% 6%;
+    }
+
+    .image-wrapper {
+      height: 300px;
+      width: 450px;
+
+      .image-content {
+        img {
+          height: 300px;
+          width: 450px;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 580px) {
+    .wrapper {
+      flex-direction: column-reverse;
+      padding: 5% 8%;
+    }
+
+    .image-wrapper {
+      width: 100%;
+      .image-content {
+        img {
+          height: 100%;
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/components/projects/link.component.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  export let link: string;
+  export let name: string;
+  export let nd2: boolean;
+</script>
+
+<a
+  class="link"
+  class:link-nd2={nd2}
+  href={link}
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <h5>{name}</h5>
+  <img
+    class="icon"
+    class:icon-nd2={nd2}
+    src="/svg/projects/enter.svg"
+    alt="link"
+  />
+</a>
+
+<style lang="scss">
+  $first-font-title-color: #c3c1c0;
+  $first-icon-color: invert(91%) sepia(7%) saturate(59%) hue-rotate(336deg)
+    brightness(86%) contrast(91%);
+
+  $second-font-title-color: #4f4d4c;
+  $second-icon-color: invert(28%) sepia(8%) saturate(143%) hue-rotate(335deg)
+    brightness(96%) contrast(86%);
+
+  .link {
+    color: $first-font-title-color;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 5px;
+
+    img {
+      $size: 30px;
+      height: $size;
+      width: $size;
+    }
+  }
+  .link-nd2 {
+    color: $second-font-title-color;
+  }
+
+  .icon {
+    filter: $first-icon-color;
+  }
+  .icon-nd2 {
+    filter: $second-icon-color;
+  }
+
+  @media (max-width: 1210px) {
+    h5 {
+      font-size: 15px;
+    }
+  }
+</style>
